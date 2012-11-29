a3c4499bd5f0e72fefbc6c8a632dcef8a29e6447
Zepho <zepho@zepho-mac-pro.local>
2012-11-28 17:22:13 -0800

    Make the script generic for any domain

--- deploy/encrypt_decrypt.rb
+++ deploy/encrypt_decrypt.rb
@@ -4,34 +4,35 @@ require 'digest/sha1'
 require 'highline/import'
 require 'clipboard'
 
-# www.rubyplus.com server credentials
+# Recall server credentials
 
-def unlock_password(account)
+def unlock_password(account, domain)
   salt = ask("Enter your secret key : ") do |q|
     q.echo = false
     q.verify_match = true
     q.gather = {"Enter your secret key" => '', "" => ''}
   end
   
-  root_password = Digest::SHA1.hexdigest(account + salt)
-  Clipboard.copy(root_password)  
+  password = Digest::SHA1.hexdigest(domain + account + salt)
+  Clipboard.copy(password)  
 end
 
 choose do |menu|
+  domain = ask("Enter the domain : ")
   menu.prompt = "Please make a selection : "
   
   menu.choice :root do 
-    unlock_password('rubyplus.com+root')
+    unlock_password('root', domain)
     say("Root password copied.") 
   end
 
   menu.choice :mysql do 
-    unlock_password('rubyplus.com+mysql')
+    unlock_password('mysql', domain)
     say("MySQL password copied.") 
   end
 
   menu.choice :deploy do 
-    unlock_password('rubyplus.com+deploy')
+    unlock_password('deploy', domain)
     say("Deploy password copied.") 
   end
   
