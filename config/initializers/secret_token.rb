7e520360e173942abbb1ff733a556b44b0531b84
Zepho <zepho@zepho-mac-pro.local>
2012-11-30 12:54:29 -0800

    First version of Cahor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cahor::Application.config.secret_token = '4a6b1bc2836685bce1a3cff4c13c34d015364242cfbf1dc221cbdff2a266f328b77dacb7b26787fdc4cc2f14b3cc86702959c6cbfa152980fa06b11d2ea15a22'
