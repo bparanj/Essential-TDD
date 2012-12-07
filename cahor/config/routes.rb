f91224764a00b479321b9870c94ee16277246d72
Zepho <zepho@zepho-mac-pro.local>
2012-12-07 12:35:17 -0800

    Pass referrer_code for custom pass through variable

--- cahor/config/routes.rb
+++ cahor/config/routes.rb
@@ -5,9 +5,9 @@ Cahor::Application.routes.draw do
   resources :orders, :only => [:new, :create] 
   get '/express' => 'orders#express', as: :express
   
-
   match "/welcome" => "welcome#index"
   match "/affiliate_signup" => "affiliates#create"
+  match "/api" => "clicks#create"
   
   devise_for :users
 
