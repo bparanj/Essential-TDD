ae0cc20ced32f90e84d1c292773e5b106915a38c
Zepho <zepho@zepho-mac-pro.local>
2012-12-12 15:56:24 -0800

    Separate logger for Paypal IPN handler

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,15 +1,6 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
-  
-  # TODO : Should the invoice be changed to order_id ? Check it by testing the checkout process manually.
-  # def create
-  #   PaymentNotification.create!(details:        params, 
-  #                               invoice:        params[:invoice], 
-  #                               status:         params[:payment_status], 
-  #                               transaction_id: params[:txn_id])
-  #   render nothing: true
-  # end
-  
+    
   def create
     ipn = InstantPaymentNotification.new(request.raw_post)
 
