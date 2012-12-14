da0608f918110561840498289a459fb00330f5e3
Zepho <zepho@zepho-mac-pro.local>
2012-12-13 18:27:00 -0800

    Hooked up refund processing

--- cahor/app/models/product.rb
+++ cahor/app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ActiveRecord::Base
   attr_accessible :price, :name
   belongs_to :user
   
-  # has_many :paypal_notifications Do you need this? Refer railscast.
-  
   def self.price_in_cents(product_id)
     product = find(product_id)
     product.price * 100
