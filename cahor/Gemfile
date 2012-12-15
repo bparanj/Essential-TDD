16570f300b318afd58e71344f0f7d450bc0f3c20
Bala Paranj <bala.paranj@zepho.com>
2012-12-14 16:36:23 -0800

    Turned off test unit file generation

--- cahor/Gemfile
+++ cahor/Gemfile
@@ -21,6 +21,7 @@ gem 'activemerchant', :require => 'active_merchant'
 gem 'rails-footnotes', '>= 3.7.5.rc4', :group => :development
 
 group :development, :test do
+  gem 'rspec-rails'
   # gem 'capybara'
   # gem 'capybara-webkit'
    
--- cahor/config/application.rb
+++ cahor/config/application.rb
@@ -62,6 +62,16 @@ module Cahor
     
     # Turn off generating css.sass and js.coffee files
     config.generators.stylesheets = false
-    config.generators.javascripts = false    
+    config.generators.javascripts = false
+    
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+    end    
   end
 end
