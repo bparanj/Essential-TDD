e02baab9169a897bd2c108857888aa8e8cbf2350
Bala Paranj <bala.paranj@zepho.com>
2012-12-14 17:22:56 -0800

    Implemented process_bounty

--- cahor/app/models/bounty.rb
+++ cahor/app/models/bounty.rb
@@ -1,6 +1,5 @@
 class Bounty < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :affiliate
-  # belongs_to :paypal_notification
   belongs_to :payable
 end
--- cahor/db/migrate/20121203212249_create_bounties.rb
+++ cahor/db/migrate/20121203212249_create_bounties.rb
@@ -2,14 +2,14 @@ class CreateBounties < ActiveRecord::Migration
   def change
     create_table :bounties do |t|
       t.integer  :affiliate_id,   :null => false
-      t.integer  :paypal_notification_id,        :null => false
-      t.integer  :payable_id,     :null => false
-      t.integer  :product_price,  :null => false
-      # amount is calculated field based on commission
+      t.integer  :payable_id,   
+      t.decimal  :product_price,      :precision => 11, :scale => 2, :null => false
+      
+      # DECISION: amount is calculated field based on commission rate
+      # DECISION: commission rate is left to the seller and they will multiply it by that amount and make 
+      # payment. So it is out of scope for now.
       
       t.timestamps
     end
   end
 end
-
-
