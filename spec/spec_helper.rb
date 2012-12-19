043435e84fcd571c568ca55441ff14fba184c087
Bala Paranj <bala.paranj@zepho.com>
2012-12-19 11:51:16 -0800

    Added paypal notifications mapper specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,3 +36,8 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 end
+
+def http_raw_data
+  "custom=89CVZ&mc_gross=500.00&address_status=confirmed&payer_id=EVMXCLDZJV77Q&tax=0.00&address_street=164+Waverley+Street&payment_date=15%3A23%3A54+Apr+15%2C+2005+PDT&payment_status=Completed&address_zip=K2P0V6&first_name=Tobias&mc_fee=15.05&address_country_code=CA&address_name=Tobias+Luetke&notify_version=1.7&payer_status=unverified&business=biz%40clickplan.net&address_country=Canada&address_city=Ottawa&quantity=1&payer_email=payer%40snowdevil.ca&verify_sign=AEt48rmhLYtkZ9VzOGAtwL7rTGxUAoLNsuf7UewmX7UGvcyC3wfUmzJP&txn_id=6G996328CK404320L&payment_type=instant&last_name=Luetke&address_state=Ontario&receiver_email=receiver%40clickplan.net&payment_fee=&receiver_id=UQ8PDYXJZQD9Y&txn_type=web_accept&item_name=Essential+TDD&item_number=1&mc_currency=CAD&test_ipn=1&payment_gross=&shipping=0.00&invoice=R180"
+end 
+
