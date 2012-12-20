3fe20105030a64c0cf62ba5b75a291d623f96610
Bala Paranj <bala.paranj@zepho.com>
2012-12-20 15:10:10 -0800

    Added billing response mapper specs

--- lib/billing_response_mapper.rb
+++ lib/billing_response_mapper.rb
@@ -4,22 +4,22 @@ class BillingResponseMapper
   end
   
   def email
-    response.email
+    @response.email
   end
   
   def first_name
-    response.params['first_name']
+    @response.params['first_name']
   end
   
   def last_name
-    response.params['last_name']
+    @response.params['last_name']
   end
   
   def custom
-    response.params['custom']
+    @response.params['custom']
   end
   
   def details
-    response.params  
+    @response.params  
   end
 end
