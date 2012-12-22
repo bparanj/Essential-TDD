76ab61935c31595297c767c510fd782902574c7c
Bala Paranj <bala.paranj@zepho.com>
2012-12-21 17:48:07 -0800

    First stab at downloads controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Cahor::Application.routes.draw do
 
   resources :products
   resources :orders, :only => [:new, :create] 
+  
   get '/express' => 'orders#express', as: :express
+  get '/downloads/:confirmation_number' => 'downloads#show'
   
   match "/welcome" => "welcome#index"
   match "/affiliate_signup" => "affiliates#create"
