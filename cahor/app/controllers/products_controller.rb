f42767f239967c462fe8fb5f41651577fefb9d1e
Bala Paranj <bparanj@gmail.com>
2012-12-25 01:10:00 -0800

    Added css for the site

--- cahor/app/controllers/products_controller.rb
+++ cahor/app/controllers/products_controller.rb
@@ -2,29 +2,14 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @products }
-    end
   end
 
   def show
     @product = Product.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @product }
-    end
   end
 
   def new
     @product = Product.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @product }
-    end
   end
 
   def edit
