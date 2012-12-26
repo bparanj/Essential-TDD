36f6c6a513af41dcb041197c7045eab8ec08ba54
Bala Paranj <bparanj@gmail.com>
2012-12-25 19:20:41 -0800

    First stab at My Profile tab. Added * for required fields

--- cahor/config/routes.rb
+++ cahor/config/routes.rb
@@ -7,7 +7,12 @@ Cahor::Application.routes.draw do
   end
   
   resources :orders, :only => [:new, :create] 
-
+  
+  get '/profile' => 'profiles#show'
+  get '/profile/new' => 'profiles#new'
+  get '/profile/edit' => 'profiles#edit'
+  put '/profile' => 'profiles#update'
+  
   get '/landing_pages_home' => 'landing_pages#home'
   get '/express' => 'orders#express', as: :express
   get '/downloads/:confirmation_number' => 'downloads#show'
