7d86799e056ede203fe0d8764555d74b04aa4b73
Bala Paranj <bparanj@gmail.com>
2012-12-25 18:05:07 -0800

    Tab selection highlight. Landing pages model and controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def is_active?(page_name)
+    logger.info("Page name #{page_name}")
+    logger.info("Controller name #{params[:controller]}")
+    servlet = params[:controller]
+    if servlet == page_name
+      "active"
+    else
+      ""
+    end
+  end
 end
--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -1,6 +1,8 @@
 class LandingPage < ActiveRecord::Base
-  # attr_accessible :title, :body
-  belongs_to :product
+  attr_accessible :link, :name, :product_id
+  
   validates_presence_of :name, :link
-
+  
+  belongs_to :product
+  # TODO: Create a before save to strip out http:// in the link
 end
