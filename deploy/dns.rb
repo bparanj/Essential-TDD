969cf5ee821429e9af41bd9c5b6c6c49f5a9c3cd
Bala Paranj <bparanj@gmail.com>
2013-01-26 21:29:57 -0800

    Updated dns script

--- deploy/dns.rb
+++ deploy/dns.rb
@@ -1,3 +1,7 @@
+ $rvm use 1.9.3-p286
+ $rvm gemset use deploy
+
+
 require 'fog'
 
 dns = Fog::DNS.new(provider: 'linode',
@@ -11,4 +15,14 @@ record = zone.records.create(value: '192.155.81.222', name: 'clickplan.net', typ
 
 # To make www.clickplan.net go to the same place, use a cname record:
 record = zone.records.create(value: 'clickplan.net', name: 'www', type: 'CNAME')
+
+# dns.zones.all
+
+# @dns = Fog::DNS.new(:provider => 'Linode', :linode_api_key => LINODE_KEY)
+# if @zone = @dns.zones.all.find { |z| z.domain == ZONE }
+#   puts "Found zone #{@zone.inspect}"
+# else
+#   @zone = @dns.zones.create(:domain => ZONE, :email  => ZONE_EMAIL)
+#   puts "Creating zone #{@zone.inspect}"
+# end
 	
